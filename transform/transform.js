7b9c7983edf75f9a066934fffb65c082d81de11e
Anneke Miers <annekemiers.am@gmail.com>
2021-04-08 16:10:33 -0600

    Add assets

--- transform/transform.js
+++ transform/transform.js
@@ -1,26 +1,62 @@
-/**
- **
- **  const zoos = {
- **    lincolnParkZoo: [
- **      {class: 'Mammal', name: 'Kangaroo'},
- **      {class: 'Reptile', name: 'Crocodile'},
- **      …
- **    ],
- **    sanDiegoZoo: [
- **      {class: 'Bird', name: 'Penguin'},
- **      {class: 'Mammal', name: 'Tiger'},
- **      …
- **    ],
- **    detroitZoo: [
- **      {class: 'Fish', name: 'Clown Fish'},
- **      {class: 'Amphibian', name: 'Salamander'},
- **      …
- **    ],
- **    …
- **  }
- **
- **/
+// const zoos = {
+//   lincolnParkZoo: [
+//     { class: 'Mammal', name: 'Kangaroo' },
+//     { class: 'Reptile', name: 'Crocodile' },
+//     { class: 'Reptile', name: 'Alligator' },
+//   ],
+//   sanDiegoZoo: [
+//     { class: 'Bird', name: 'Penguin' },
+//     { class: 'Mammal', name: 'Tiger' },
+//     { class: 'Reptile', name: 'Alligator' },
 
+//   ],
+//   detroitZoo: [
+//     { class: 'Fish', name: 'Clown Fish' },
+//     { class: 'Amphibian', name: 'Salamander' },
+//     { class: 'Mammal', name: 'Tiger' },
+//     { class: 'Reptile', name: 'Alligator' },
+//     { class: 'Mammal', name: 'Capybara' },
+//   ],
+// }
+
+
+const organizeAnimalsByClass = () => {
+  let organizedAnimals = {};
+  Object.values(zoos).forEach(zoo => {
+    zoo.forEach(animal => {
+      if (!organizedAnimals[animal.class]) {
+        organizedAnimals[animal.class] = [];
+      }
+      if (!organizedAnimals[animal.class].includes(animal.name)) {
+        organizedAnimals[animal.class].push(animal.name)
+      }
+    })
+  })
+  Object.values(organizedAnimals).forEach(animalName => animalName.sort());
+  return organizedAnimals;
+}
+
+organizeAnimalsByClass();
+
+
+// On my first attempt, I had the sort() inside of the forEach. I think running the sort every time a new animal is added would slow down the process when dealing with a large data set, so I moved the sort outside of the forEach.
+
+//  Pseudo Code-
+// Input: a zoo object with zoo arrays of animal objects
+// Output: object one key per animal class with values of animals in alpha order, no duplicates
+// Methods: object.keys, forEach, reduce, sort
+// How: 
+// create a new object for end result
+//  for each zoo, for each class, check if class value is in new object,
+      // if it is not in there, add it to the object w/ value of []
+      // Next, check values
+          //value not already existing inside of the {} inside of it's key? push
+          //value is? skip
+// Then check for sorting order
+
+
+
+// Prompt - 
 // Transform the "zoos" object so that animals across every zoo are organized
 // by "class" and alphabetized by "name". Try to make the transformation
 // efficient as you may have to manipulate a very large dataset.
